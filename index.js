a8b1b356a85eee4c6e43fbe9179cfc3a58661f72
74ultra <r.barnhill@protonmail.com>
2020-12-29 23:38:35 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+// calling express like a function generates a running express application
+
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' })
+})
+// route handler
+
+const PORT = process.env.PORT || 5000
+// if there is an environment variable that has already been defined, use it. If not, use 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
